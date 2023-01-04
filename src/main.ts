a0816fbabb98140c68ffe23656646e94d009b7b0
Diablohu <Diablohu@users.noreply.github.com>
2023-01-04 12:08:27 +0800

    more error handlers

--- src/main.ts
+++ src/main.ts
@@ -56,6 +56,23 @@ let launched = false;
         .catch((err) => {
             console.log('\n');
             console.error(err);
+
+            if (err.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log(err.response?.data);
+                console.log(err.response?.status);
+                console.log(err.response?.headers);
+            } else if (err.request) {
+                // The request was made but no response was received
+                // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+                // http.ClientRequest in node.js
+                console.log(err.request);
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log('Error', err.message);
+            }
+            console.log(err.config);
         });
 
     launched = true;
