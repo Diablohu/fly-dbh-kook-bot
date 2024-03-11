4130bebd145cef14e1b89f65080b8f5ab671e434
Diablohu <huxin@cmcm.com>
2024-03-11 20:52:28 +0800

    updating test file

--- build-copy/text.cjs
+++ build-copy/text.cjs
@@ -1,5 +1,20 @@
+function prepareEnvKey(key) {
+    if (!process.env[`${key}`]) {
+        process.env[`${key}`] =
+            process.env[`${key.toLowerCase()}`] ||
+            (!!process.env[`${key}_FILE`] &&
+            fs.existsSync(process.env[`${key}_FILE`] || '')
+                ? fs.readFileSync(process.env[`${key}_FILE`] || '', 'utf-8')
+                : '');
+    }
+}
+
 function main() {
     console.log('test');
+
+    prepareEnvKey('KOOK_TOKEN');
+    prepareEnvKey('AVWX_TOKEN');
+
     console.log(process.env);
 }
 
