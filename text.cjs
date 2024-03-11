f47ea2cbcf354e31c49e69577007db06a472ae70
Diablohu <Diablohu@users.noreply.github.com>
2024-03-12 01:24:35 +0800

    DIABLOHU_PUBLISH 2024/3/12 01:24:35

--- text.cjs
+++ text.cjs
@@ -1,1 +1,23 @@
-import fs from"fs";function prepareEnvKey(e){process.env[`${e}`]||(process.env[`${e}`]=process.env[`${e.toLowerCase()}`]||(process.env[`${e}_FILE`]&&fs.existsSync(process.env[`${e}_FILE`]||"")?fs.readFileSync(process.env[`${e}_FILE`]||"","utf-8"):""))}function main(){console.log("test"),prepareEnvKey("KOOK_TOKEN"),prepareEnvKey("AVWX_TOKEN"),console.log(process.env)}main();
+const fs = require('fs');
+
+function prepareEnvKey(key) {
+    if (!process.env[`${key}`]) {
+        process.env[`${key}`] =
+            process.env[`${key.toLowerCase()}`] ||
+            (!!process.env[`${key}_FILE`] &&
+            fs.existsSync(process.env[`${key}_FILE`] || '')
+                ? fs.readFileSync(process.env[`${key}_FILE`] || '', 'utf-8')
+                : '');
+    }
+}
+
+function main() {
+    console.log('test');
+
+    prepareEnvKey('KOOK_TOKEN');
+    prepareEnvKey('AVWX_TOKEN');
+
+    console.log(process.env);
+}
+
+main();
