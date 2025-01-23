68cb4ce2a2c11892ac3d10560bada13969ca084d
Diablohu <huxin@cmcm.com>
2025-01-23 18:46:19 +0800

    update deps

--- src/helpers/transform-string-to-kmarkdown.ts
+++ src/helpers/transform-string-to-kmarkdown.ts
@@ -3,11 +3,12 @@ import { regexStringUrlPattern } from '../vars';
 function transformStringToKMarkdown(input: string): string {
     return input.replace(
         new RegExp(`(.?)${regexStringUrlPattern}(.?)`, 'g'),
-        (match, p1, p2, p3) => {
+        (match, p1, p2, ...args) => {
+            // console.log(123, { match, p1, p2, args, p3: args.at(-3) });
             // 如果匹配前后是空格，无视
-            if (p1 === '(' && p3 === ')') return match;
+            if (p1 === '(' && args.at(-3) === ')') return match;
             if (p1 === '(' && p2.slice(-1) === ')') return match;
-            return `${p1}[${p2}](${p2})${p3}`;
+            return `${p1}[${p2}](${p2})${args.at(-3)}`;
         },
     );
 }
